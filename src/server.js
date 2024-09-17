1080b88bbd7b05966f500410f1ac004cfdf6c174
Dylan Daccarett (RIT Student) <dcd7867@rit.edu>
2024-09-17 18:36:49 -0400

    jeepers

--- src/server.js
+++ src/server.js
@@ -2,16 +2,6 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const textHandler = require('./textResponses.js');
 const jsonHandler = require('./jsonResponses.js');
-<<<<<<< HEAD
-
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
-const onRequest = (request, response) => {
-    console.log(request.url);
-};
-
-http.createServer(onRequest).listen(port, () => {
-   console.log(`Listening on 127.0.0.1:${port}`);
-=======
 const imageHandler = require('./imageResponses.js');
 
 const port = process.env.PORT || process.env.NODE.PORT || 3000;
@@ -65,5 +55,4 @@ const onRequest = (request, response) => {
 
 http.createServer(onRequest).listen(port, () => {
   console.log(`Listening on 127.0.0.1:${port}`);
->>>>>>> 5933887240bddae1c0547b61b2dae0e762e6d629
 });
